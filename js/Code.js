3d422213fc0392eb4da30e656a3c65fe207fdf51
ranveer2005 <77142090+ranveer2005@users.noreply.github.com>
2021-07-28 16:43:26 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.    fill("white")
+    textSize(15)
+    text("U F N C I T O N", 700,150)
+    fill("lightblue")
+    text("Hint: Logarithmic,trignometric.. ", 100,170)
+
+    fill("white")
+    textSize(15)
+    text(" A A D B A T S E", 100,300)
+    fill("lightblue")
+    text("Hint: firebase", 100,320)
+
+
+    
+
+}
